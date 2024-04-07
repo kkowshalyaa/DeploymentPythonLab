e99ea3bd3fbdaee1448630958503d25f3e8c93ee
Kowshalyaa Kumaraguru <76553801+kkowshalyaa@users.noreply.github.com>
2024-04-07 13:28:40 +0530

    Update startDockerPython.py

--- startDockerPython.py
+++ startDockerPython.py
@@ -25,4 +25,32 @@ stdin, stdout, stderr = sshcon.exec_command("sudo docker build -t python-lab Pyt
 
 
 
+# libraries to be imported 
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+fromaddr = "EMAIL address of the sender"
+toaddr = "EMAIL address of the receiver"
+msg = MIMEMultipart() 
+msg['From'] = fromaddr 
+msg['To'] = toaddr 
+msg['Subject'] = "Subject of the Mail"
+body = "Body_of_the_mail"
+msg.attach(MIMEText(body, 'plain')) 
+filename = "File_name_with_extension"
+attachment = open("Path of the file", "rb") 
+p = MIMEBase('application', 'octet-stream') 
+p.set_payload((attachment).read()) 
+encoders.encode_base64(p) 
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+msg.attach(p) 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+s.starttls() 
+s.login(fromaddr, "Password_of_the_sender") 
+text = msg.as_string() 
+s.sendmail(fromaddr, toaddr, text) 
+s.quit() 
+
 
