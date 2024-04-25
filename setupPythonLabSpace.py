c0332e52abf5d9ba83a6aeaa905760f59dbf0f86
Kowshalyaa Kumaraguru <76553801+kkowshalyaa@users.noreply.github.com>
2024-04-26 00:05:25 +0530

    Update setupPythonLabSpace.py

--- setupPythonLabSpace.py
+++ setupPythonLabSpace.py
@@ -15,7 +15,7 @@ ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', output)[0]
 
 #Establish SSH Connection to EC2 Instance
 myuser   = 'ubuntu'
-mySSHK   = '/root/keys/' + sys.argv[0] + '.key'
+mySSHK   = '/home/ubuntu/DeploymentPythonLab/Keys/' + sys.argv[0] + '.key'
 sshcon   = paramiko.SSHClient() 
 sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 sshcon.connect(ip, username=myuser, key_filename=mySSHK)
