4164e87b8a8cd867958ecc55ac7dae98413fce00
Kowshalyaa Kumaraguru <76553801+kkowshalyaa@users.noreply.github.com>
2024-04-26 00:48:31 +0530

    Update provider.tf

--- TerrformDeploy/provider.tf
+++ TerrformDeploy/provider.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAVFONGMWYKGKMUJ7H"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
