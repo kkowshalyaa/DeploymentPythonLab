cc2f8c6859906c31b07533c08f9745894233fd40
Kowshalyaa Kumaraguru <76553801+kkowshalyaa@users.noreply.github.com>
2024-04-26 10:58:36 +0530

    Update generateKey.sh

--- generateKey.sh
+++ generateKey.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                        
-if [ -e /home/ubuntu/DeploymentPythonLab/Keys/$1.key  ]                                                          
+if [ -e /home/ubuntu/DeploymentPythonLab/Keys/$1.pem  ]                                                          
 then                                                                               
   echo "Key Exists"                                                                
 else                                                                               
-  ssh-keygen -t rsa -N "" -f /home/ubuntu/DeploymentPythonLab/Keys/$1.key                                        
+  ssh-keygen -m PEM -t rsa -b 8192 -N "" -f /home/ubuntu/DeploymentPythonLab/Keys/$1.pem                                        
 fi 
